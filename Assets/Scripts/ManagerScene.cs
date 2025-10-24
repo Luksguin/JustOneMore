0c7e9f56d20712260450f6745849f8191b3cc1fb
Lucas Albuquerque <guinho.lucas7@gmail.com>
2025-10-24 15:14:06 -0300

    add level 3 and add transition between scenes

--- Assets/Scripts/ManagerScene.cs
+++ Assets/Scripts/ManagerScene.cs
@@ -1,15 +1,36 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.UI;
+using System.Collections;
+
 
 public class ManagerScene : MonoBehaviour
 {
+    public Image transitionImage;
+    public float duration;
+
+    public AudioSource myAudioSource;
+
     public void ChangeScene(int nextScene)
     {
-        SceneManager.LoadScene(nextScene);
+        if(transitionImage)StartCoroutine(TransitionCoroutine(nextScene));
+        else SceneManager.LoadScene(nextScene);
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    IEnumerator TransitionCoroutine(int nextScene)
+    {
+        transitionImage.gameObject.SetActive(true);
+        transitionImage.DOColor(new Color(0f, 0f, 0f, 1f), duration);
+        myAudioSource.Play();
+
+        yield return new WaitForSeconds(duration + 2f);
+
+        SceneManager.LoadScene(nextScene);
     }
 }
